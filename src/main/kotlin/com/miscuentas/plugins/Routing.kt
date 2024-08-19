bad31d6ee281144a90b98b6dc3f0576c01c90b76
LeoCare <81829356+LeoCare@users.noreply.github.com>
2024-08-19 11:33:27 +0200

    agregar rutas a Routing

--- src/main/kotlin/com/miscuentas/plugins/Routing.kt
+++ src/main/kotlin/com/miscuentas/plugins/Routing.kt
@@ -1,11 +1,20 @@
 package com.miscuentas.plugins
 
-import com.miscuentas.routes.usuarioRoute
+import com.miscuentas.routes.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         usuarioRoute()
+        balanceRoute()
+        gastoRoute()
+        hojaRoute()
+        imagenRoute()
+        pagoRoute()
+        participanteRoute()
+        tipoPerfilRoute()
+        tipoStatusRoute()
+        tipoBalanceRoute()
     }
 }
